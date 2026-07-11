34080955c05dfcfe02c23dc5487a776adc822cd7
naveed-devz <snaveedn23@gmail.com>
2026-07-11 17:37:38 +0530

    fixed manifest dynamic issues

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { siteDescription, siteName } from "@/lib/site";
 
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: siteName,
